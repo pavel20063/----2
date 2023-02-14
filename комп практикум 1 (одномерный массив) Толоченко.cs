169fb1c3128af394cb8dce254dd673caf1408180
pavel20063 <123302689+pavel20063@users.noreply.github.com>
2023-02-14 12:30:56 +0200

    Add files via upload

---
+++ комп практикум 1 (одномерный массив) Толоченко.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace PC.work1___Tolochenko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ЗАДАНИЕ 1
+            //int[] Work1 = new int[6];
+            //for (int i = 0; i < Work1.Length; i++)
+            //{
+            //    Console.Write("Please,write number of the element {0}", i);
+            //    Work1[i] = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("array:");
+            //foreach (int elem in Work1)
+            //{
+            //    Console.Write("{0} ", elem);
+            //}
+
+
+            //ЗАДАНИЕ 2
+            //int[] Work2 = { 2, 4, 6, 8, 10, 12 };
+            //for (int i = 0; i < Work2.Length; i++)
+            //{
+            //    Console.WriteLine(Work2[i]);
+            //}
+
+            //ЗАДАНИЕ 3
+            //int[] work3 = new int[20];
+            //for (int i = 0; i < work3.Length; i++)
+            //{
+            //    work3[i] = i * i - 1;
+            //}
+            //foreach (int value in work3)
+            //{
+            //    Console.WriteLine(value);
+            //}
+
+
+            //ЗАДАНИЕ 4
+            //int[] work4 = new int[15];
+            //Random random = new Random();
+            //for (int i = 0; i < work4.Length; i++)
+            //{
+            //    work4[i] = random.Next(100);
+            //}
+            //foreach (int randomvalue in work4)
+            //{
+            //    Console.WriteLine(randomvalue);
+            //}
+
+
+
+        }
+    }
+}
